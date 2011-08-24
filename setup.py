54ec272effc009eb89a690891345b93ee69e9a90
Richard Bucker <richard@bucker.net>
2011-08-24 12:49:46 +0000

    added some test cases; updated the doc and tweaked the setup.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ config = {
     'download_url': 'https://rbucker881@github.com/rbucker881/sub-watcher.git',
     'author_email': 'richard@bucker.net',
     'version': '0.1',
-    'install_requires': ['python-redis-log','python-zeromq-log','pyzmq','redis-py'],
+    'install_requires': ['nose','python-redis-log','python-zeromq-log','pyzmq','redis-py'],
     'packages': ['sub-watcher'],
     'scripts': [],
     'name': 'sub-watcher'
